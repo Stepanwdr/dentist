de0577e3effc6b235b75733c8fab0982c0170bf0
Stepanwdr <stepanwdr@gmail.com>
2026-04-26 16:57:26 +0400

    aded res

--- server/app.js
+++ server/app.js
@@ -13,6 +13,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.get('/', (req, res) => {
+  res.send('API WORKS');
+});
 
 // middlewares
 app.use(headers);
