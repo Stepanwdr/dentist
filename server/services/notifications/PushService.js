585da0f21736bd187702c37611fa295933056efb
Stepanwdr <stepanwdr@gmail.com>
2026-04-26 21:25:23 +0400

    firebase key changed

--- server/services/notifications/PushService.js
+++ server/services/notifications/PushService.js
@@ -2,9 +2,12 @@ import admin from "firebase-admin";
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
 
 // Инициализация Firebase (один раз!)
 if (!admin.apps.length) {
