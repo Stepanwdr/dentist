ae6ba3f6ce3778ca0796664b837692f25fe1fb16
Stepanwdr <stepanwdr@gmail.com>
2026-04-30 21:28:19 +0400

    statuses

--- server/services/notifications/PushService.js
+++ server/services/notifications/PushService.js
@@ -2,7 +2,7 @@ import admin from "firebase-admin";
 import fs from "fs";
 
 const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("./dentist-37300-firebase-adminsdk-fbsvc-c001320a70.json", import.meta.url))
+  fs.readFileSync(new URL("./dentist-37300-firebase-adminsdk-fbsvc-2140c3d655.json", import.meta.url))
 );
 
 
